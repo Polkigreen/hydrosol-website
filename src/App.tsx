03f3fc1c9b53874df3b46082b0ec2946a129cb97
HydroSol <polkigreen@gmail.com>
2025-01-10 21:30:56 +0100

    Added App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Reviews from './components/Reviews';
 import Contact from './components/Contact';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import './App.css';
 
 function App() {
   const [language, setLanguage] = useState('en');
